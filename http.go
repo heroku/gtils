be02faf0431ff00bc37dcbef2a0ec5a6a265e925
Andrew Gwozdziewycz <apg@heroku.com>
2015-05-27 15:31:23 -0400

    Allow for disabling of EnsureHTTPS functionality via env
    - Add a test

--- http.go
+++ http.go
@@ -1,10 +1,18 @@
 package gtils
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
+// EnsureHTTPS wraps a HTTP handler and ensures it was requested over HTTPS.
+//
+// If DISABLED_ENSURE_HTTPS is in the environment and set to either "1" or "true", EnsureHTTPS will always pass.
 func EnsureHTTPS(handler http.HandlerFunc) http.HandlerFunc {
+	tmp := os.Getenv("DISABLE_ENSURE_HTTPS")
+	disabled := tmp == "1" || tmp == "true"
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Scheme != "https" && r.Header.Get("X-Forwarded-Proto") != "https" {
+		if !disabled && (r.URL.Scheme != "https" && r.Header.Get("X-Forwarded-Proto") != "https") {
 			w.WriteHeader(http.StatusForbidden)
 			return
 		}
